362ba4437cbf0449891b74e582e2ce4edc3147b2
unknown <thabisorankhala@gmail.com>
2024-04-29 04:17:44 +0200

    collected static files for render

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -118,6 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "static"] # new
+STATIC_ROOT = BASE_DIR / "staticfiles" # new
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
